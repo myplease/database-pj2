d41b413b39144d8ed975708a18b2f9b3356f1706
zhangyanxun <22302010069@m.fudan.edu.cn>
2024-06-23 12:35:17 +0800

    fix : trigger bug let change data err

--- src/Data/Test.java
+++ src/Data/Test.java
@@ -7,13 +7,16 @@ public class Test {
         Database database = new Database("root","Zhang2004");
 //        database.userOrderDish(1,1,new int[]{3,4},new int[]{3,3},true);
 //        System.out.println(database.getMerchantIdByPhone_Number("00000000000"));
-        ArrayList<String[]> rows = database.userShowPriceHistory(1);
-        for (int i = 0; i < rows.size(); i++) {
-            for (int j = 0; j < rows.get(i).length; j++) {
-                System.out.print(rows.get(i)[j]);
-                System.out.print(" ");
-            }
-            System.out.println();
-        }
+//        database.merchantAddDish(1,"1","1","1","1","1","1");
+        database.userOrderDish(1,1,new int[]{1,10},new int[]{1,2},false);
+        database.userPutScoreOnOrderDish(4,10,5);
+//        ArrayList<String[]> rows = database.userShowPriceHistory(1);
+//        for (int i = 0; i < rows.size(); i++) {
+//            for (int j = 0; j < rows.get(i).length; j++) {
+//                System.out.print(rows.get(i)[j]);
+//                System.out.print(" ");
+//            }
+//            System.out.println();
+//        }
     }
 }
